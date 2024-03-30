d0a4a35e69ed8beadece4debf1945ca3dfb502de
Guigo <rsbecker@inf.ufsm.br>
2024-03-29 22:46:07 -0300

    Estilizar seção 'Projetos'

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/sobre.css';
+import '../styles/components/projetos.css';
 import '../styles/utils.css';
