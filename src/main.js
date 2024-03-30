160998befb8057fef81f2f21e9674c4930a4bc45
Guigo <rsbecker@inf.ufsm.br>
2024-03-30 09:26:02 -0300

    Estilizar seção 'Contato'

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/sobre.css';
 import '../styles/components/projetos.css';
+import '../styles/components/contato.css';
 import '../styles/utils.css';
